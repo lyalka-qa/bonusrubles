1b8b83ac8c74d7df37e18619c0335cd57c12bd4b
Alena Filatova <ppasd2005@yandex.ru>
2021-03-22 10:17:41 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount1 = 357;
+        int amount2 = 1050;
+        int rubles = 100;
+        int bonus = 1;
+        int total = (amount1 + amount2) / rubles * bonus;
+        System.out.println(total);
+    }
+}
