7a49b887afeff51d55c4aeb8b8588a5dce41bdc7
Alena Filatova <ppasd2005@yandex.ru>
2021-03-22 15:19:29 +0300

    Modified 2

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int transfer = 1357;
+        int transfer = 1255;
         int rubles = 100;
         int bonus = 1;
-        int finalBalance = balance + transfer;
-        if (finalBalance >= 1000) {
+        if (transfer >= 1000) {
             bonus = 1;
                     }
         else {
             bonus = 0;
         }
-        int totalBonus = bonus * finalBalance / rubles;
+        int totalBonus = bonus * transfer / rubles;
+        int totalBalance = balance + transfer + totalBonus;
         System.out.println(totalBonus);
+        System.out.println(totalBalance);
     }
 }
